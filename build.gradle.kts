aa13d4bbcb0c87101d84e5cc4feaf4a46920781e
Arid Alimudin <arid.alimudin22@gmail.com>
2026-09-09 10:52:37 +0700

    Fix: correct cloudstream plugin group ID to com.github.recloudstream:gradle:-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.2")
-        classpath("com.github.recloudstream.gradle:gradle:81b1d424d")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
